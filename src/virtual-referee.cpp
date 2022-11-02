a7bfce3e97c5afc17cc124dbf4a24f62888084c0
Gabriel Lucas <53130753+UmloucoBR@users.noreply.github.com>
2022-11-01 23:08:10 -0300

    Added a pause button and another way to draw contours

--- src/virtual-referee.cpp
+++ src/virtual-referee.cpp
@@ -15,6 +15,8 @@ int main(int argc, char* argv[]) {
 	cv::Ptr<cv::BackgroundSubtractor> pMOG2 = cv::createBackgroundSubtractorMOG2(500, 16.0, false);
 	std::vector<cv::Vec2f> lines;
 	std::vector<cv::Vec3f> circles;
+	//std::vector<std::vector<cv::Point>> contours;
+	//std::vector<cv::Vec4i> hierarchy;
 	bool pause {false};
 
 	while (!pause)
@@ -29,9 +31,24 @@ int main(int argc, char* argv[]) {
 		cv::cvtColor(vid2, vid2Gray, cv::COLOR_BGR2GRAY);
 
 		cv::Canny(vid2Gray, vid2Canny, 50, 200, 3, true);
-		cv::HoughLines(vid2Canny, lines, 1, CV_PI / 180, 150, 0, 0);
+		cv::HoughLines(vid2Canny, lines, 1, CV_PI / 180, 200, 0, 0);
 
 		pMOG2->apply(vid2, pMOG2Mask);
+
+		/*
+		cv::dilate(pMOG2Mask, pMOG2Mask, 1);
+		cv::HoughCircles(pMOG2Mask, circles, cv::HOUGH_GRADIENT, 1.5, 400, 100.0, 100.0, 80, 250);
+
+		for (size_t i = 0; i < circles.size(); ++i) {
+			cv::circle(
+				vid2,
+				cv::Point(cvRound(circles[i][0]), cvRound(circles[i][1])),
+				cvRound(circles[i][2]),
+				cv::Scalar(0, 0, 255),
+				2
+			);
+		}*/
+
 		cv::cvtColor(pMOG2Mask, pMOG2Mask, cv::COLOR_GRAY2BGR);
 
 		for (int i = 0; i < lines.size(); i++)
@@ -56,23 +73,18 @@ int main(int argc, char* argv[]) {
 			cv::line(pMOG2Mask, pt1, pt2, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
 		}
 
-		cv::HoughCircles(vid2Canny, circles, cv::HOUGH_GRADIENT, 2.0, vid2Canny.cols / 10);
-
-		for (size_t i = 0; i < circles.size(); ++i) {
-			cv::circle(
-				vid2,
-				cv::Point(cvRound(circles[i][0]), cvRound(circles[i][1])),
-				cvRound(circles[i][2]),
-				cv::Scalar(0, 0, 255),
-				2
-			);
-		} 
+		/*findContours(pMOG2Mask, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+		cv::Mat drawing = cv::Mat::zeros(pMOG2Mask.size(), CV_8UC3);
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			cv::Scalar color = cv::Scalar(10, 10, 10);
+			drawContours(vid2, contours, static_cast<int>(i), color, 2, cv::LINE_8, hierarchy, 0);
+		}*/
 
 		cv::imshow("Line Detector", pMOG2Mask);
 		cv::imshow("Line Detector 2", vid2);
 
 		int ascii {static_cast<int>(cv::waitKey(33))};
-
 		if (ascii == 112)
 		{
 			while (cv::waitKey(0) != 112)
