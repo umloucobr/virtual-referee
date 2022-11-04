74509df0af363572cd9fb4c2da2b6d51b23e0576
Gabriel Lucas <53130753+UmloucoBR@users.noreply.github.com>
2022-11-04 14:13:02 -0300

    Included individual libraries instead of all of them

--- src/virtual-referee.h
+++ src/virtual-referee.h
@@ -1,7 +1,13 @@
 ﻿#pragma once
 #ifndef VIRTUAL_REFEREE_H
 #define VIRTUAL_REFEREE_H
-#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp> //GUI elements.
+#include <opencv2/core/types.hpp> //cv::Mat and cv::Size.
+#include <opencv2/videoio.hpp> //cv::VideoCapture and cv::VideoWriter.
+#include <opencv2/imgcodecs.hpp> //cv::imread and cv::imwrite.
+#include <opencv2/video/background_segm.hpp> //cv::BackgroundSubtractor.
+#include <opencv2/imgproc.hpp> //Image processing.
+#include <iostream>
 #include <string>
 #include <vector>
 #include <queue>
